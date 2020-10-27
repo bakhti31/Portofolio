3777d224564ea444233b57a1486358ab72de7ec1
bakhti31 <bakhtiar.rf@gmail.com>
2020-10-28 00:03:49 +0800

    First Commit

---
+++ index.php
@@ -0,0 +1,152 @@
+<?php 
+session_start();
+
+$page = $_GET['p'];
+
+
+
+ ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<link rel="stylesheet" href="dist/css/bootstrap.css">
+	<link rel="stylesheet" href="dist/css/style.css">
+</head>
+<body class="bg-dark">
+	<div class="container-fluid">
+		<div class="row text-center bg-dark w-100 py-1 position-fixed" style="z-index: 1;">
+			<div class="col">
+				<a href="" class="btn btn-dark w-100">Home</a>
+			</div>
+			<div class="col">
+				<a href="" class="btn btn-dark w-100">Blog</a>
+			</div>
+			<div class="col">
+				<a href="" class="btn btn-dark w-100">Files</a>
+			</div>
+			<div class="col">
+				<a href="" class="btn btn-dark w-100">About</a>
+			</div>
+			<div class="col-1">
+				<a href="" class="btn btn-dark w-100">Login</a>
+			</div>
+		</div>
+		<!-- <div class="jumbotron bg-dark" style="z-index: 1;"> -->
+			<div class="card py-3 bg-dark btn-dark" style="z-index: 2;">
+			  <div class="row no-gutters">
+			    <div class="col-md-3 text-center">
+			      <img src="dist/images/me.jpg" class="card-img" style="max-width: 100%;" alt="Bakhtiar Ramadhan Photo">
+			    </div>
+			    <div class="col-md-9">
+			      <div class="card-body">
+			        <p class="card-text">
+			        	Hi, Welcome There. <br>
+			        	My Name Is Bakhtiar Ramadhan <br>
+			        	There is My Place to take my Project of Life <br>
+			        	I want make this place be my diary too :D <br>
+			        	<div class="row contact">
+			        		<a class="col-4 whatsapp align-self-end text-center">
+			        			<img src="dist/images/wa.png" alt="Whatsapp" style="max-width: 40%;">
+			        			<h5>082297177440</h5>
+			        		</a>
+			        		<a href="http://fb.com/bakhtiar.rf" class="col-4 facebook align-self-end text-center">
+			        			<img src="dist/images/fb.png" alt="Facebook" style="max-width: 40%">
+			        			<h5>Bakhtiar Ramadhan</h5>
+			        		</a>
+			        		<a href="" class="col-4 github align-self-end text-center">
+			        			<img src="dist/images/gith.png" alt="Github" style="max-width: 40%">
+			        			<h5>Bakhti31</h5>
+			        		</a>
+			        	</div>
+			        </p>
+
+			      </div>
+			    </div>
+			  </div>
+			</div>
+		<!-- </div> -->
+		<div class="container btn-dark">
+			<div class="row text-center py-1 w-100">
+				<div class="col">
+					<a href="" class="btn btn-dark w-100">Home</a>
+				</div>
+				<div class="col">
+					<a href="" class="btn btn-dark w-100">Blog</a>
+				</div>
+				<div class="col">
+					<a href="" class="btn btn-dark w-100">Files</a>
+				</div>
+				<div class="col">
+					<a href="" class="btn btn-dark w-100">About</a>
+				</div>
+				<div class="col-1">
+					<a href="" class="btn btn-dark w-100">Login</a>
+				</div>
+			</div>
+			<table>
+				<tr>
+					<td>asdlkghj</td>
+					<td>lakdhg</td>
+					<td>alsdkgh</td>
+				</tr>
+				<tr>
+					<td>alsdkhj</td>
+					<td>alsdkgh</td>
+					<td>aldfkgh</td>
+				</tr>
+				<tr>
+					<td>lasdkgh</td>
+					<td>lakdfhg</td>
+					<td>ldkfgh</td>
+				</tr>
+				<tr>
+					<td>aldfkgh</td>
+					<td>adlfkgh</td>
+					<td>adflghk</td>
+				</tr>
+				<tr>
+					<td>adlfkgh</td>
+					<td>adflkgh</td>
+					<td>adlghk</td>
+				</tr>
+				<tr>
+					<td>ldfkgh</td>
+					<td>ldkasfgh</td>
+					<td>alk</td>
+				</tr>
+				<tr>
+					<td>lsdfkgh</td>
+					<td>sldfkgh</td>
+					<td>sdlfkgh</td>
+				</tr>
+				<tr>
+					<td>sdklfgh</td>
+					<td>ldfkgh</td>
+					<td>sldkh</td>
+				</tr>
+			</table>
+			Lorem ipsum dolor sit amet consectetur adipisicing, elit. Modi officiis, eos voluptatum. Ipsum distinctio reiciendis veniam iure fugiat, dignissimos ad deserunt in architecto tenetur amet quis voluptate, magnam, dolorum nobis.
+			Accusamus omnis sapiente exercitationem et, pariatur fuga enim a aliquid adipisci laboriosam, obcaecati accusantium eum voluptas repellat cupiditate officia maiores reiciendis officiis totam consequatur rerum? Laborum totam illo, aut voluptas?
+			Non magni aut, libero aspernatur delectus itaque veritatis deleniti. Labore similique, enim sequi assumenda! Minima harum labore pariatur aperiam dicta assumenda ipsam, sapiente molestias vitae facere repudiandae ab veniam nisi?
+			Ad praesentium id veritatis esse ab eos ex eligendi, possimus perspiciatis, magni asperiores, provident voluptatum facere. Pariatur illo quisquam exercitationem, esse, corporis, suscipit dolor, quos nemo repudiandae perferendis eos tempore.
+			Deserunt asperiores in, cumque consequuntur corporis voluptas nulla vero sed esse qui eius ducimus illum quam eligendi dicta impedit repellendus quaerat officia tempore minus ipsam aliquid possimus quibusdam voluptate fugit!
+			Nam, ad. Impedit dolor optio maxime, quas necessitatibus et temporibus error odit porro molestiae veniam pariatur. Laboriosam a aspernatur nisi maiores cum. Laudantium amet est dolores voluptas accusamus architecto, odit?
+			Id enim tempore iusto eaque, mollitia. Perspiciatis eaque aut, autem saepe eveniet dicta molestias laborum voluptate dolor pariatur ipsa laboriosam esse, quos, illo veniam natus ut corrupti sunt magnam minus.
+			Necessitatibus explicabo est recusandae maiores inventore perspiciatis qui iste eligendi? Voluptatem deleniti possimus voluptas distinctio quo excepturi. Eius dicta, harum repellendus nesciunt architecto! Tempore eum alias nulla, blanditiis quam ratione.
+			Ab ad commodi labore odit libero atque non corporis laudantium sed laboriosam tempora, possimus minus repellendus ex est ipsam consectetur a vero iste rerum cum nam dolorem. Tenetur, dolores deleniti.
+			Ea recusandae quis corporis expedita, explicabo aut necessitatibus quam officia illum, perspiciatis tempora unde at fuga sapiente debitis, qui. Id, incidunt sed ipsa beatae vitae temporibus deleniti, architecto magni possimus.
+			Id assumenda facere optio iusto alias sunt exercitationem fugiat adipisci? Voluptates, totam, voluptatem. Odit ipsa, maiores deleniti aspernatur adipisci maxime sapiente laboriosam quod rem quaerat ut. Obcaecati nam blanditiis, amet.
+			Minus totam ad ipsum mollitia quisquam culpa perspiciatis, explicabo architecto recusandae, voluptate. Quae fugit sint, sapiente accusamus nobis quasi ullam. Natus quam quaerat est itaque, labore earum ipsa, aliquid eaque?
+			Dignissimos facilis sed, debitis veritatis. Cumque tenetur assumenda doloribus molestias doloremque accusantium dignissimos explicabo inventore commodi iusto dolorem ex ullam, optio non illum hic similique reprehenderit obcaecati ducimus, officia, iste!
+			Quo quia culpa, aliquid eius est, nobis reprehenderit quibusdam impedit vero ea. Eum est perspiciatis ab dolor tempora, quo omnis placeat harum nulla corrupti laboriosam illum veritatis fuga illo eveniet.
+			Quo eveniet praesentium tenetur quas eaque molestias non aliquid nam debitis animi, quae? Nam aliquid commodi vel in aliquam quae natus nostrum, dolore quasi ipsam reprehenderit iure accusamus maxime non.
+			Enim facilis beatae cumque eaque doloremque adipisci, molestiae, aliquam, ea numquam magnam dolores ipsam modi veritatis. Odio magni nulla fuga eaque distinctio expedita quos ratione voluptatem, nihil, reprehenderit repudiandae est.
+		</div>
+	</div>
+</body>
+<script type="text/javascript" src="dist/js/jquery.js"></script>
+<script type="text/javascript" src="dist/js/bootstrap.js"></script>
+</html>
